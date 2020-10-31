9673d4bce1f4303b69a41c9b89066d81f5b18295
sureshhmrc <27204388+sureshhmrc@users.noreply.github.com>
2020-10-31 18:48:34 +0000

    fix tests

--- test/controllers/PayPeriodsControllerSpec.scala
+++ test/controllers/PayPeriodsControllerSpec.scala
@@ -16,119 +16,131 @@
 
 package controllers
 
+import java.time.LocalDate
+
 import base.SpecBaseControllerSpecs
+import forms.PayPeriodsFormProvider
+import models.{ClaimPeriod, PayFrequency, PayPeriods, Period, UserAnswers}
+import pages.{ClaimPeriodPage, EndPayDatePage, LastPayDatePage, PayFrequencyPage, PayPeriodsPage}
+import play.api.libs.json.{JsString, Json}
+import play.api.test.Helpers._
+import views.html.PayPeriodsView
+
+import scala.concurrent.ExecutionContext.Implicits.global
 
 class PayPeriodsControllerSpec extends SpecBaseControllerSpecs {
 
-//  val view                             = app.injector.instanceOf[PayPeriodsView]
-//  private lazy val payPeriodsRouteGet  = routes.PayPeriodsController.onPageLoad().url
-//  private lazy val payPeriodsRoutePost = routes.PayPeriodsController.onSubmit().url
-//
-//  private val formProvider = new PayPeriodsFormProvider()
-//  private val form         = formProvider()
-//
-//  def controller(userAnswers: Option[UserAnswers]) = new PayPeriodsController(
-//    messagesApi,
-//    mockSessionRepository,
-//    navigator,
-//    getSessionAction,
-//    stubDataRetrieval(userAnswers),
-//    dataRequired,
-//    formProvider,
-//    component,
-//    view
-//  )
-//
-//  val claimPeriod  = ClaimPeriod.Nov2020
-//  val payFrequency = PayFrequency.Monthly
-//  val lastPayDate  = "2020-10-30"
-//
-//  val userAnswers = UserAnswers(
-//    userAnswersId,
-//    Json.obj(
-//      ClaimPeriodPage.toString  -> JsString(claimPeriod),
-//      PayFrequencyPage.toString -> JsString(payFrequency),
-//      LastPayDatePage.toString  -> JsString(lastPayDate)
-//    )
-//  )
-//  val periods     = List(Period(LocalDate.of(2020, 10, 31), LocalDate.of(2020, 11, 30)))
+  val view                             = app.injector.instanceOf[PayPeriodsView]
+  private lazy val payPeriodsRouteGet  = routes.PayPeriodsController.onPageLoad().url
+  private lazy val payPeriodsRoutePost = routes.PayPeriodsController.onSubmit().url
 
-//  "PayPeriods Controller" must {
-//
-//    "return OK and the correct view for a GET" in {
-//      val request = fakeRequest(GET, payPeriodsRouteGet)
-//
-//      val result = controller(Some(userAnswers)).onPageLoad()(request)
-//
-//      status(result) mustEqual OK
-//
-//      contentAsString(result) mustEqual
-//        view(form, periods)(request, messages).toString
-//    }
-//
-//    "populate the view correctly on a GET when the question has previously been answered" in {
-//
-//      val userAnswersUpdated = userAnswers.set(PayPeriodsPage, PayPeriods.values.head).success.value
-//
-//      val request = fakeRequest(GET, payPeriodsRouteGet)
-//
-//      val result = controller(Some(userAnswersUpdated)).onPageLoad()(request)
-//
-//      status(result) mustEqual OK
-//
-//      contentAsString(result) mustEqual
-//        view(form.fill(PayPeriods.values.head), periods)(request, messages).toString
-//    }
-//
-//    "redirect to the next page when valid data is submitted" in {
-//
-//      val request =
-//        fakeRequest(POST, payPeriodsRoutePost)
-//          .withFormUrlEncodedBody(("value", PayPeriods.values.head.toString))
-//
-//      val result = controller(Some(userAnswers)).onSubmit()(request)
-//
-//      status(result) mustEqual SEE_OTHER
-//
-//      redirectLocation(result).value mustEqual routes.SelectWorkPeriodsController.onPageLoad().url
-//    }
-//
-//    "return a Bad Request and errors when invalid data is submitted" in {
-//
-//      val request =
-//        fakeRequest(POST, payPeriodsRoutePost)
-//          .withFormUrlEncodedBody(("value", "invalid value"))
-//
-//      val boundForm = form.bind(Map("value" -> "invalid value"))
-//
-//      val result = controller(Some(userAnswers)).onSubmit()(request)
-//
-//      status(result) mustEqual BAD_REQUEST
-//
-//      contentAsString(result) mustEqual
-//        view(boundForm, periods)(request, messages).toString
-//    }
-//
-//    "redirect to Session Expired for a GET if no existing data is found" in {
-//
-//      val request = fakeRequest(GET, payPeriodsRouteGet)
-//
-//      val result = controller(None).onPageLoad()(request)
-//
-//      status(result) mustEqual SEE_OTHER
-//      redirectLocation(result).value mustEqual routes.SessionExpiredController.onPageLoad().url
-//    }
-//
-//    "redirect to Session Expired for a POST if no existing data is found" in {
-//
-//      val request =
-//        fakeRequest(POST, payPeriodsRoutePost)
-//          .withFormUrlEncodedBody(("value", PayPeriods.values.head.toString))
-//
-//      val result = controller(None).onSubmit()(request)
-//      status(result) mustEqual SEE_OTHER
-//
-//      redirectLocation(result).value mustEqual routes.SessionExpiredController.onPageLoad().url
-//    }
-//  }
+  private val formProvider = new PayPeriodsFormProvider()
+  private val form         = formProvider()
+
+  def controller(userAnswers: Option[UserAnswers]) = new PayPeriodsController(
+    messagesApi,
+    mockSessionRepository,
+    navigator,
+    getSessionAction,
+    stubDataRetrieval(userAnswers),
+    dataRequired,
+    formProvider,
+    component,
+    view
+  )
+
+  val claimPeriod  = ClaimPeriod.Nov2020
+  val payFrequency = PayFrequency.Monthly
+  val lastPayDate  = "2020-10-30"
+  val endPayDate   = "2020-11-30"
+
+  val userAnswers = UserAnswers(
+    userAnswersId,
+    Json.obj(
+      ClaimPeriodPage.toString  -> JsString(claimPeriod),
+      PayFrequencyPage.toString -> JsString(payFrequency),
+      LastPayDatePage.toString  -> JsString(lastPayDate),
+      EndPayDatePage.toString   -> JsString(endPayDate)
+    )
+  )
+  val periods     = List(Period(LocalDate.of(2020, 10, 31), LocalDate.of(2020, 11, 30)))
+
+  "PayPeriods Controller" must {
+
+    "return OK and the correct view for a GET" in {
+      val request = fakeRequest(GET, payPeriodsRouteGet)
+
+      val result = controller(Some(userAnswers)).onPageLoad()(request)
+
+      status(result) mustEqual OK
+
+      contentAsString(result) mustEqual
+        view(form, periods)(request, messages).toString
+    }
+
+    "populate the view correctly on a GET when the question has previously been answered" in {
+
+      val userAnswersUpdated = userAnswers.set(PayPeriodsPage, PayPeriods.values.head).success.value
+
+      val request = fakeRequest(GET, payPeriodsRouteGet)
+
+      val result = controller(Some(userAnswersUpdated)).onPageLoad()(request)
+
+      status(result) mustEqual OK
+
+      contentAsString(result) mustEqual
+        view(form.fill(PayPeriods.values.head), periods)(request, messages).toString
+    }
+
+    "redirect to the next page when valid data is submitted" in {
+
+      val request =
+        fakeRequest(POST, payPeriodsRoutePost)
+          .withFormUrlEncodedBody(("value", PayPeriods.values.head.toString))
+
+      val result = controller(Some(userAnswers)).onSubmit()(request)
+
+      status(result) mustEqual SEE_OTHER
+
+      redirectLocation(result).value mustEqual routes.SelectWorkPeriodsController.onPageLoad().url
+    }
+
+    "return a Bad Request and errors when invalid data is submitted" in {
+
+      val request =
+        fakeRequest(POST, payPeriodsRoutePost)
+          .withFormUrlEncodedBody(("value", "invalid value"))
+
+      val boundForm = form.bind(Map("value" -> "invalid value"))
+
+      val result = controller(Some(userAnswers)).onSubmit()(request)
+
+      status(result) mustEqual BAD_REQUEST
+
+      contentAsString(result) mustEqual
+        view(boundForm, periods)(request, messages).toString
+    }
+
+    "redirect to Session Expired for a GET if no existing data is found" in {
+
+      val request = fakeRequest(GET, payPeriodsRouteGet)
+
+      val result = controller(None).onPageLoad()(request)
+
+      status(result) mustEqual SEE_OTHER
+      redirectLocation(result).value mustEqual routes.SessionExpiredController.onPageLoad().url
+    }
+
+    "redirect to Session Expired for a POST if no existing data is found" in {
+
+      val request =
+        fakeRequest(POST, payPeriodsRoutePost)
+          .withFormUrlEncodedBody(("value", PayPeriods.values.head.toString))
+
+      val result = controller(None).onSubmit()(request)
+      status(result) mustEqual SEE_OTHER
+
+      redirectLocation(result).value mustEqual routes.SessionExpiredController.onPageLoad().url
+    }
+  }
 }
--- test/controllers/SelectWorkPeriodsControllerSpec.scala
+++ test/controllers/SelectWorkPeriodsControllerSpec.scala
@@ -16,137 +16,150 @@
 
 package controllers
 
+import java.time.LocalDate
+
 import base.SpecBaseControllerSpecs
+import forms.SelectWorkPeriodsFormProvider
+import models.PayFrequency.Monthly
+import models.{ClaimPeriod, PayFrequency, Period, UserAnswers}
+import pages.{ClaimPeriodPage, EndPayDatePage, LastPayDatePage, PayFrequencyPage, SelectWorkPeriodsPage}
+import play.api.libs.json.{JsString, Json}
+import play.api.test.Helpers._
+import views.html.SelectWorkPeriodsView
+
+import scala.concurrent.ExecutionContext.Implicits.global
 
 class SelectWorkPeriodsControllerSpec extends SpecBaseControllerSpecs {
 
-//  val view = app.injector.instanceOf[SelectWorkPeriodsView]
-//
-//  private lazy val selectWorkPeriodsRouteGet  = routes.SelectWorkPeriodsController.onPageLoad().url
-//  private lazy val selectWorkPeriodsRoutePost = routes.SelectWorkPeriodsController.onSubmit().url
-//
-//  private val formProvider = new SelectWorkPeriodsFormProvider()
-//  private val form         = formProvider()
-//
-//  val claimPeriod  = ClaimPeriod.Nov2020
-//  val payFrequency = PayFrequency.FortNightly
-//  val lastPayDate  = "2020-10-31"
-//
-//  val userAnswers = UserAnswers(
-//    userAnswersId,
-//    Json.obj(
-//      ClaimPeriodPage.toString  -> JsString(claimPeriod),
-//      PayFrequencyPage.toString -> JsString(payFrequency),
-//      LastPayDatePage.toString  -> JsString(lastPayDate)
-//    )
-//  )
-//
-//  val period1 = Period(LocalDate.of(2020, 11, 1), LocalDate.of(2020, 11, 14))
-//  val period2 = Period(LocalDate.of(2020, 11, 15), LocalDate.of(2020, 11, 28))
-//
-//  val periods = List(period1, period2)
-//
-//  def controller(userAnswers: Option[UserAnswers]) = new SelectWorkPeriodsController(
-//    messagesApi,
-//    mockSessionRepository,
-//    navigator,
-//    getSessionAction,
-//    stubDataRetrieval(userAnswers),
-//    dataRequired,
-//    formProvider,
-//    component,
-//    view
-//  )
-//
-//  "SelectWorkPeriods Controller" must {
-//
-//    "return OK and the correct view for a GET" in {
-//
-//      val request = fakeRequest(GET, selectWorkPeriodsRouteGet)
-//
-//      val result = controller(Some(userAnswers)).onPageLoad()(request)
-//
-//      status(result) mustEqual OK
-//
-//      contentAsString(result) mustEqual
-//        view(form, periods)(request, messages).toString
-//    }
-//
-//    "populate the view correctly on a GET when the question has previously been answered" in {
-//
-//      val userAnswersUpdated = userAnswers.set(SelectWorkPeriodsPage, periods).success.value
-//
-//      val request = fakeRequest(GET, selectWorkPeriodsRouteGet)
-//
-//      val result = controller(Some(userAnswersUpdated)).onPageLoad()(request)
-//
-//      status(result) mustEqual OK
-//
-//      contentAsString(result) mustEqual
-//        view(form.fill(periods), periods)(request, messages).toString
-//    }
-//
-//    "redirect to /regular-pay-amount when only one period exist for a GET" in {
-//
-//      val request = fakeRequest(GET, selectWorkPeriodsRouteGet)
-//
-//      val result = controller(Some(userAnswers.set(PayFrequencyPage, Monthly).success.value)).onPageLoad()(request)
-//
-//      status(result) mustEqual SEE_OTHER
-//
-//      redirectLocation(result).value mustEqual routes.RegularPayAmountController.onPageLoad().url
-//    }
-//
-//    "redirect to the next page when valid data is submitted" in {
-//
-//      val request =
-//        fakeRequest(POST, selectWorkPeriodsRoutePost)
-//          .withFormUrlEncodedBody(("value[0]", LocalDate.now().toString + "_" + LocalDate.now().plusDays(1).toString))
-//
-//      val result = controller(Some(userAnswers)).onSubmit()(request)
-//
-//      status(result) mustEqual SEE_OTHER
-//
-//      redirectLocation(result).value mustEqual routes.RegularPayAmountController.onPageLoad().url
-//    }
-//
-//    "return a Bad Request and errors when invalid data is submitted" in {
-//
-//      val request =
-//        fakeRequest(POST, selectWorkPeriodsRoutePost)
-//          .withFormUrlEncodedBody(("value", "invalid value"))
-//
-//      val boundForm = form.bind(Map("value" -> "invalid value"))
-//
-//      val result = controller(Some(userAnswers)).onSubmit()(request)
-//
-//      status(result) mustEqual BAD_REQUEST
-//
-//      contentAsString(result) mustEqual
-//        view(boundForm, periods)(request, messages).toString
-//    }
-//
-//    "redirect to Session Expired for a GET if no existing data is found" in {
-//
-//      val request = fakeRequest(GET, selectWorkPeriodsRouteGet)
-//
-//      val result = controller(None).onPageLoad()(request)
-//
-//      status(result) mustEqual SEE_OTHER
-//      redirectLocation(result).value mustEqual routes.SessionExpiredController.onPageLoad().url
-//    }
-//
-//    "redirect to Session Expired for a POST if no existing data is found" in {
-//
-//      val request =
-//        fakeRequest(POST, selectWorkPeriodsRoutePost)
-//          .withFormUrlEncodedBody(("value[0]", LocalDate.now().toString))
-//
-//      val result = controller(None).onSubmit()(request)
-//
-//      status(result) mustEqual SEE_OTHER
-//
-//      redirectLocation(result).value mustEqual routes.SessionExpiredController.onPageLoad().url
-//    }
-//  }
+  val view = app.injector.instanceOf[SelectWorkPeriodsView]
+
+  private lazy val selectWorkPeriodsRouteGet  = routes.SelectWorkPeriodsController.onPageLoad().url
+  private lazy val selectWorkPeriodsRoutePost = routes.SelectWorkPeriodsController.onSubmit().url
+
+  private val formProvider = new SelectWorkPeriodsFormProvider()
+  private val form         = formProvider()
+
+  val claimPeriod  = ClaimPeriod.Nov2020
+  val payFrequency = PayFrequency.FortNightly
+  val lastPayDate  = "2020-10-31"
+  val endPayDate   = "2020-11-30"
+
+  val userAnswers = UserAnswers(
+    userAnswersId,
+    Json.obj(
+      ClaimPeriodPage.toString  -> JsString(claimPeriod),
+      PayFrequencyPage.toString -> JsString(payFrequency),
+      LastPayDatePage.toString  -> JsString(lastPayDate),
+      EndPayDatePage.toString   -> JsString(endPayDate)
+    )
+  )
+
+  val period1 = Period(LocalDate.of(2020, 11, 1), LocalDate.of(2020, 11, 14))
+  val period2 = Period(LocalDate.of(2020, 11, 15), LocalDate.of(2020, 11, 28))
+
+  val periods = List(period1, period2)
+
+  def controller(userAnswers: Option[UserAnswers]) = new SelectWorkPeriodsController(
+    messagesApi,
+    mockSessionRepository,
+    navigator,
+    getSessionAction,
+    stubDataRetrieval(userAnswers),
+    dataRequired,
+    formProvider,
+    component,
+    view
+  )
+
+  "SelectWorkPeriods Controller" must {
+
+    "return OK and the correct view for a GET" in {
+
+      val request = fakeRequest(GET, selectWorkPeriodsRouteGet)
+
+      val result = controller(Some(userAnswers)).onPageLoad()(request)
+
+      status(result) mustEqual OK
+
+      contentAsString(result) mustEqual
+        view(form, periods)(request, messages).toString
+    }
+
+    "populate the view correctly on a GET when the question has previously been answered" in {
+
+      val userAnswersUpdated = userAnswers.set(SelectWorkPeriodsPage, periods).success.value
+
+      val request = fakeRequest(GET, selectWorkPeriodsRouteGet)
+
+      val result = controller(Some(userAnswersUpdated)).onPageLoad()(request)
+
+      status(result) mustEqual OK
+
+      contentAsString(result) mustEqual
+        view(form.fill(periods), periods)(request, messages).toString
+    }
+
+    "redirect to /regular-pay-amount when only one period exist for a GET" in {
+
+      val request = fakeRequest(GET, selectWorkPeriodsRouteGet)
+
+      val result = controller(Some(userAnswers.set(PayFrequencyPage, Monthly).success.value)).onPageLoad()(request)
+
+      status(result) mustEqual SEE_OTHER
+
+      redirectLocation(result).value mustEqual routes.RegularPayAmountController.onPageLoad().url
+    }
+
+    "redirect to the next page when valid data is submitted" in {
+
+      val request =
+        fakeRequest(POST, selectWorkPeriodsRoutePost)
+          .withFormUrlEncodedBody(("value[0]", LocalDate.now().toString + "_" + LocalDate.now().plusDays(1).toString))
+
+      val result = controller(Some(userAnswers)).onSubmit()(request)
+
+      status(result) mustEqual SEE_OTHER
+
+      redirectLocation(result).value mustEqual routes.RegularPayAmountController.onPageLoad().url
+    }
+
+    "return a Bad Request and errors when invalid data is submitted" in {
+
+      val request =
+        fakeRequest(POST, selectWorkPeriodsRoutePost)
+          .withFormUrlEncodedBody(("value", "invalid value"))
+
+      val boundForm = form.bind(Map("value" -> "invalid value"))
+
+      val result = controller(Some(userAnswers)).onSubmit()(request)
+
+      status(result) mustEqual BAD_REQUEST
+
+      contentAsString(result) mustEqual
+        view(boundForm, periods)(request, messages).toString
+    }
+
+    "redirect to Session Expired for a GET if no existing data is found" in {
+
+      val request = fakeRequest(GET, selectWorkPeriodsRouteGet)
+
+      val result = controller(None).onPageLoad()(request)
+
+      status(result) mustEqual SEE_OTHER
+      redirectLocation(result).value mustEqual routes.SessionExpiredController.onPageLoad().url
+    }
+
+    "redirect to Session Expired for a POST if no existing data is found" in {
+
+      val request =
+        fakeRequest(POST, selectWorkPeriodsRoutePost)
+          .withFormUrlEncodedBody(("value[0]", LocalDate.now().toString))
+
+      val result = controller(None).onSubmit()(request)
+
+      status(result) mustEqual SEE_OTHER
+
+      redirectLocation(result).value mustEqual routes.SessionExpiredController.onPageLoad().url
+    }
+  }
 }
